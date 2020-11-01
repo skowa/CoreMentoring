94e8d62dbeff82ebb85fe0a6b385523b532fb99b
Aleksandra <skowa98@gmail.com>
2020-11-01 23:37:27 +0300

    Task 1 was added

---
+++ Northwind.Web.Tests.Unit/Builders/ProductEditModelBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Northwind.Web.Models;
+
+namespace Northwind.Web.Tests.Unit.Builders
+{
+    internal class ProductEditModelBuilder
+    {
+        private readonly ProductEditModel _productModel = new ProductEditModel();
+
+        internal ProductEditModelBuilder WithProductName(string productName)
+        {
+            _productModel.ProductName = productName;
+
+            return this;
+        }
+
+        internal ProductEditModel Build()
+        {
+            return _productModel;
+        }
+    }
+}
---
+++ Northwind.Web.Tests.Unit/Builders/ProductModelBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Northwind.Web.Models;
+
+namespace Northwind.Web.Tests.Unit.Builders
+{
+    internal class ProductModelBuilder
+    {
+        private readonly ProductModel _productModel = new ProductModel();
+
+        internal ProductModelBuilder WithProductName(string productName)
+        {
+            _productModel.ProductName = productName;
+
+            return this;
+        }
+
+        internal ProductModel Build()
+        {
+            return _productModel;
+        }
+    }
+}
